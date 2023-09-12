f28f42b974f9186eefe83a7f37ae1e998d878787
matin <elliotmigh@gmail.com>
2023-09-12 16:08:51 +0330

    add binding in build.gradle and create object from binding set binding root in MainActivity.kt

--- app/src/main/java/com/example/sanjabfile/MainActivity.kt
+++ app/src/main/java/com/example/sanjabfile/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sanjabfile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.sanjabfile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //create binding:
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
